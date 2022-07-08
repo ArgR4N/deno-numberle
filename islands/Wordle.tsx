c3dfa6a24c67e8962ae4a350c7e086558e004f01
ramiro <ramiroreinaldo12@gmail.com>
2022-07-07 23:06:12 -0300

    General changes in islands/Wordle.tsx

--- islands/Wordle.tsx
+++ islands/Wordle.tsx
@@ -1,25 +1,26 @@
 /** @jsx h */
 import { h } from "preact";
-import { useState, useEffect, useRef } from "preact/hooks";
+import { useState, useEffect, useRef, useContext} from "preact/hooks";
 import {tw} from '@twind'
+
+//Ts =>
+import { NumberSpace, Configuration } from "../routes/index.tsx"
+
+//Components =>
 import NumberBox from "./NumberBox.tsx"
 
 //Preact type=>
 type StateUpdater<S> = (value: S | ((prevState: S) => S)) => void;
 
-
 //Const´s =>
-const MAX_TRIES = 5
-const NUMBER_LENGTH = 4
 const NUMBERS = Array(10).fill('').map((_ , i) => String(i));
-const MAX = Math.pow(10, NUMBER_LENGTH) - 1
-const MIN = Math.pow(10, NUMBER_LENGTH - 1)
-const NUMBER = Math.floor(Math.random() * (MAX - MIN) + MIN)
 
 //Interfaces =>
-interface WordleProps { tries: number }
+//interface WordleProps { tries: number }
 interface Key{ key: string }
-
+interface WordleProps{
+  CONFIG: Configuration
+}
 //Types =>
 type Handler = (key: { key: string} ) => void;
 type KeyHandler = (key: string) => void
@@ -45,73 +46,41 @@ const useEventListener = (eventName: string, handler: Handler) => {
 //Main function =>
 export default function Wordle( { CONFIG }:WordleProps ) {
   const { MAX_TRIES, NUMBER_LENGTH, NUMBER } = CONFIG
-//Classes =>
-class ActualGuessPair {
-  first: number;
-  second: number;
-
-  constructor(first: number, second: number){
-    this.first = first,
-    this.second = second
-  }
-
-  addFirst(setter: StateUpdater<ActualGuessPair>){
-    this.first < MAX_TRIES - 1 && setter(new ActualGuessPair(this.first + 1, 0))
-  }
-
-  addSecond(setter: StateUpdater<ActualGuessPair>){
-    setter( 
-      new ActualGuessPair(this.first, 
-        NUMBER_LENGTH - 1 > this.second 
-          ? this.second + 1 
+  //Classes =>
+  class ActualGuessPair {
+    first: number;
+    second: number;
+  
+    constructor(first: number, second: number){
+      this.first = first,
+      this.second = second
+    }
+  
+    addFirst(setter: StateUpdater<ActualGuessPair>){
+      this.first < MAX_TRIES - 1 && setter(new ActualGuessPair(this.first + 1, 0))
+    }
+  
+    addSecond(setter: StateUpdater<ActualGuessPair>){
+      setter( 
+        new ActualGuessPair(this.first, 
+          NUMBER_LENGTH - 1 > this.second 
+            ? this.second + 1 
+            : this.second))
+    }
+  
+    minusSecond(setter: StateUpdater<ActualGuessPair>){
+      setter(      
+        new ActualGuessPair(this.first,
+          this.second > 0
+          ? this.second - 1
           : this.second))
+    }
   }
-
-  minusSecond(setter: StateUpdater<ActualGuessPair>){
-    setter(      
-      new ActualGuessPair(this.first,
-        this.second > 0
-        ? this.second - 1
-        : this.second))
-  }
-}
-
-export class NumberSpace{
-  color: string;
-  value: string;
-
-  constructor(n: string | false){
-    this.color = 'grey';
-    this.value = ''
-  }
-
-  ChangeColor(color: string): NumberSpace{
-    this.color = color
-    return this
-  }
-}
-
-//Other Functions =>
-const useEventListener = (eventName: string, handler: Handler) => {
-  const savedHandler:{ current: Handler } = useRef();
-
-  useEffect(() => {
-    savedHandler.current = handler;
-  }, [handler]);
-
-  useEffect(() => {
-    const eventListener = (event: { key : string }) => savedHandler.current(event);
-    globalThis.addEventListener(eventName, eventListener);
-    return () => {
-      globalThis.removeEventListener(eventName, eventListener);
-    };
-  }, [eventName, globalThis]);
-};
-
-//Main function =>
-export default function Wordle(props: WordleProps) {
-  const [previousGuesses, setPreviousGuesses] = useState(Array(MAX_TRIES).fill('').map(_ => Array(NUMBER_LENGTH).fill('').map(_ => new NumberSpace())))
-  const [actualGuess, setActualGuess] = useState(new ActualGuessPair(0, 0))
+  const [ previousGuesses, setPreviousGuesses ] = useState(
+    Array(MAX_TRIES).fill('')
+        .map(_ => Array(NUMBER_LENGTH).fill('')
+            .map(_ => new NumberSpace())))
+  const [ actualGuess, setActualGuess ] = useState(new ActualGuessPair(0, 0))
 
   const colorNumbers: (guess: NumberSpace[]) => void = (guess) =>{
     const objective = String(NUMBER)
@@ -120,23 +89,20 @@ export default function Wordle(props: WordleProps) {
       ? num.ChangeColor('green')
       : objective.includes(num.value)
         ? num.ChangeColor('yellow')
-        : num
+        : num.ChangeColor('red')
     ))
     const newGuesses = previousGuesses.slice()
     newGuesses[actualGuess.first] = newGuess
-
-      console.log(newGuess)
-
     setPreviousGuesses(newGuesses)
 
   }
   const checkWin: ( ) => boolean = ( ) => {
-    if(Number(previousGuesses[actualGuess.first][actualGuess.second]) === NUMBER){
+    if(String(previousGuesses[actualGuess.first].map(e => e.value)) == String(String(1234).split(''))){
       //Handle win =>
+      alert('you win')
     }
     return false
   }
-
   const checkLength: (n: number) => boolean = n => {
     let sum = 0
     previousGuesses[actualGuess.first].forEach(el => {
@@ -144,10 +110,10 @@ export default function Wordle(props: WordleProps) {
     })
     return sum === n
   }
-
   const checkRow:KeyHandler = () =>{
     if(checkLength(NUMBER_LENGTH)){
       colorNumbers(previousGuesses[actualGuess.first])
+      checkWin()
       actualGuess.addFirst(setActualGuess)
     }
     //if(checkWin()) endGame()
@@ -162,7 +128,7 @@ export default function Wordle(props: WordleProps) {
     else newGuess[actualGuess.first][actualGuess.second].value = ''
     setPreviousGuesses(newGuess)
   }
-  const addChar:KeyHandler = (key) =>{
+  const addChar:KeyHandler= (key) =>{
     if(!previousGuesses[actualGuess.first][actualGuess.second].value) {
       const newGuess = previousGuesses.slice()
 
@@ -178,7 +144,8 @@ export default function Wordle(props: WordleProps) {
       //Error -> Full length!
     }
   }
-  const handler: (key:Key) => void = ({ key }) => {
+  
+  const handler: Handler = ({ key }) => {
     const KeyHandler:{[name: string]: KeyHandler} = {
       'Enter': checkRow,
       'Backspace': delChar,
