b1caac2e40a0d5190342db09b69c6485232cb95a
r4n <ramiroreinaldo12@gmail.com>
2023-04-16 23:30:16 -0300

    Modified config

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,30 +1,28 @@
 /** @jsx h */
 import { h, Component } from "preact";
-import { useState, useContext } from "preact/hooks";
-import { createContext } from "preact/src"
+import { useEffect } from "preact/hooks";
 import { tw } from "@twind";
 import MainGame from "../islands/MainGame.tsx";
 
-
 //Main Classes and Interfaces =>
-export class NumberSpace{
+export class NumberSpace {
   color: string;
   value: string;
 
-  constructor(n ?: string){
-    this.color = 'gray';
-    this.value = n ? n : ''
+  constructor(n?: string) {
+    this.color = "gray";
+    this.value = n ? n : "";
   }
 
-  ChangeColor(color: string): NumberSpace{
-    this.color = color
-    return this
+  ChangeColor(color: string)  {
+    this.color = color;
+    return this;
   }
 }
 
 //  class WordleInstance extends Component{
 //     constructor(max: number, ){
-//       super() 
+//       super()
 //     }
 //     render() {
 //     return <div></div>
@@ -32,57 +30,49 @@ export class NumberSpace{
 
 //  }
 
-export interface Configuration{
-  COLORS_VALUES: {[n: string]:string},
-  NUMBER_LENGTH: number,
-  MAX_TRIES: number,
-  NUMBER: number,
-}
-// <= Main Classes and Interfaces 
+// <= Main Classes and Interfaces
 
-export const getPrime = ( n: number ) => {
-    for (let i = 2; i < n; i++) {
-        if (n % i == 0) {
-            return false;
-        }
+export const getPrime = (n: number) => {
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) {
+      return false;
     }
-  return true
-}
+  }
+  return true;
+};
 
-export const getEven = ( n:number ) => n % 2;
+export const getEven = (n: number) => n % 2;
 
-
-
-const getRandomNumber = () => Math.floor(Math.random() * (Math.pow(10, 4) - 1 - Math.pow(10, 4 - 1)) + Math.pow(10, 4 - 1))
+const getRandomNumber = () =>
+  Math.floor(
+    Math.random() * (Math.pow(10, 4) - 1 - Math.pow(10, 4 - 1)) +
+      Math.pow(10, 4 - 1)
+  );
 
 //Main Config =>
-const CONFIG:Configuration = {
-  NUMBER_LENGTH: 4,
-  MAX_TRIES: 5,
+const CONFIG = {
+  NUMBER_LENGTH: 5,
+  MAX_TRIES: 6,
   NUMBER: getRandomNumber(),
   COLORS_VALUES: {
-    'yellow':'yellow-300',
-    'red':'red-500',
-    'green':'green-200',
-    'gray':'gray-200'
-  }
-}
+    yellow: "yellow-300",
+    red: "red-500",
+    green: "green-200",
+    gray: "gray-200",
+  },
+};
 //<= Main Config
 
 export default function Home() {
-  
   return (
-    <div 
-      style={{height:'100vh'}} 
+    <div
+      style={{ height: "100vh" }}
       class={tw`bg-[#ffffff] h-full p-4 mx-auto max-w-screen-md`}
     >
       <h1 class={tw`text-center font-bold text-gray-700`}> NUMBERLE </h1>
       <hr />
 
-        <MainGame 
-          CONFIG = { CONFIG } 
-        />
-
+      <MainGame CONFIG={CONFIG} />
     </div>
   );
 }
